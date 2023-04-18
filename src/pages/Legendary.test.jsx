25e89231050879bc5acbbdf7b23c945d9185ae9e
Dev1 - filip <pelloiafilippo@live.com>
2023-04-18 16:24:34 +0200

    First commit

--- src/pages/Legendary.test.jsx
+++ src/pages/Legendary.test.jsx
@@ -1,30 +1,6 @@
 import {render, screen} from '@testing-library/react'
 import Legendary from './Legendary.jsx'
-// import {BrowserRouter} from 'react-router-dom'
 
-// const MockPokemon = (props) => {
-//     return(
-//         <BrowserRouter>
-//             <Hell titolo={props.titolo} />
-//         </BrowserRouter>
-//     )
-// }
- 
-
-// import { LensRounded } from '@mui/icons-material';
-// import handlers from './mocks/handlers.jsx'
-// import { setupServer } from 'msw/node';
-
-// const server = setupServer(...handlers)
-
-// beforeAll(() => server.listen());
-// afterAll(() => server.close());
-// afterEach(() => server.resetHandlers());
-
-// it('Should render an image, id, name, and one or two types for each pokemon-section', () => {
-//     render(<Legendary/>);
-    
-// })
 
 
 it('should be visible the section which contains the API data', () => {
@@ -39,9 +15,9 @@ it('should the header be visible', () => {
     expect(header).toBeVisible();
 })
 
-it('should change the value of the state shiny', () => {
-    render(<Legendary/>);
-    co
-})
+// it('should change the value of the state shiny', () => {
+//     render(<Legendary/>);
+//     const shinyButton = screen.get
+// })
 
 
